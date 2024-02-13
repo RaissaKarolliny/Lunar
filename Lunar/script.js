1d585d47b75c26a49fb65927fe58a65c8074f466
Anderson <115733205+andere2015@users.noreply.github.com>
2024-02-13 10:26:55 -0300

    Delete Lunar directory

--- Lunar/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    const gatilho = document.getElementById('gatilhoMenu');
-    const menu = document.getElementById('menuOffCanvas');
-    gatilho.addEventListener('click', ()=>{
-        menu.classList.toggle('ativarOffCanvas');
-    })
-}
