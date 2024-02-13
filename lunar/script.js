5bd3b4e651e9ec92fee7afd72619f2b1c7e63c1c
Hnnrik <113845766+Hnnrik@users.noreply.github.com>
2024-02-13 11:29:52 -0300

    Add files via upload

--- lunar/script.js
+++ lunar/script.js
@@ -1,7 +1,26 @@
-window.onload = function(){
+window.onload = function () {
     const gatilho = document.getElementById('gatilhoMenu');
     const menu = document.getElementById('menuOffCanvas');
-    gatilho.addEventListener('click', ()=>{
+    gatilho.addEventListener('click', () => {
         menu.classList.toggle('ativarOffCanvas');
-    })
+    });
+
 }
+function substituirFoto(imagem) {
+    var foto = document.getElementById("upLoadPhoto").getElementsByTagName("img")[0];
+    foto.src = imagem;
+}
+function handleFileSelect(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(event) {
+        var imagem = event.target.result;
+        substituirFoto(imagem);
+    };
+    reader.readAsDataURL(file);
+}
+function openFileInput() {
+    document.getElementById('fileInput').click();
+}
+
+
