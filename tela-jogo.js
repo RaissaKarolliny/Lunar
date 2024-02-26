50406c69ba9475e902b2fc9f386c97cc5670c366
Frerp <forwork929@gmail.com>
2024-02-26 20:35:15 -0300

    adicionando novas telas

---
+++ tela-jogo.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var minhaImagem = document.getElementById('descricao-jogo-imagem');
+    var meuParagrafo = document.getElementById('descricao-jogo-div-paragrafo');
+    var meuTitulo = document.getElementById('descricao-jogo-titulo');
+    var minhaAvaliacao = document.getElementById('descricao-jogo-icone-avaliacao')
+
+
+    var alturaImagem = minhaImagem.offsetHeight;
+    var alturaTitulo = meuTitulo.offsetHeight;
+    var alturaAvaliacao = minhaAvaliacao.offsetHeight;
+    var alturaParagrafo = alturaImagem - (alturaTitulo + alturaAvaliacao + 20);
+
+    meuParagrafo.style.maxHeight = alturaParagrafo + 'px';
+});
